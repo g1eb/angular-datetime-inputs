6cf605c633e9b7622b88f3b8dac291f961bcb3cd
Gleb <git@gleb.nl>
2016-07-11 10:49:42 -0400

    Split up datetime directive into three separate directives with separate templates.

--- src/datetime-input.js
+++ src/datetime-input.js
@@ -1,14 +1,15 @@
 'use strict';
 
 angular.module('g1b.datetime-input', []).
-  directive('datetimeInput', ['$document', function ($document) {
 
+/**
+ * Datetime directive (date and time input element)
+ */
+directive('datetimeInput', ['$document', function ($document) {
   return {
     restrict: 'E',
     scope: {
-      datetime: '=?',
-      date: '=?',
-      time: '=?',
+      datetime: '=',
       handler: '&'
     },
     replace: true,
@@ -46,10 +47,99 @@ angular.module('g1b.datetime-input', []).
             scope.datetime = moment(scope.datetime);
           }
 
+          // Bind click events outside directive to close edit popover
+          $document.on('mousedown', function (e) {
+            if ( !!scope.selected && !element[0].contains(e.target) ) {
+              scope.$apply(function () {
+                scope.selected = '';
+                scope.calendar_active = false;
+              });
+            }
+          });
+        }
+      };
+    }
+  };
+}]).
+
+
+/**
+ * Date directive (date input element)
+ */
+directive('dateInput', ['$document', function ($document) {
+  return {
+    restrict: 'E',
+    scope: {
+      date: '=',
+      handler: '&'
+    },
+    replace: true,
+    templateUrl: './date-input.html',
+    compile: function () {
+      return {
+        pre: function preLink() {},
+        post: function postLink(scope, element) {
+
+          // Get current date
+          scope.current = moment();
+
+          // Set selected date
+          scope.selectDate = function (date) {
+            if ( scope.selected === date ) {
+              scope.selected = undefined;
+            } else {
+              scope.selected = date;
+              scope.calendar = scope.selected.clone();
+            }
+          };
+
+          // Update selected date
+          scope.setDate = function (date, calendar_update) {
+            if ( scope.selected.isSame(date) ) { return; }
+            scope.selected.year(date.year()).month(date.month()).date(date.date()).hours(date.hours()).minutes(date.minutes()).seconds(date.seconds());
+            if ( scope.selected.clone().startOf('week').month() !== scope.calendar.month() || calendar_update ) {
+              scope.calendar = scope.selected.clone();
+            }
+            scope.handler();
+          };
+
           // Convert date object to moment.js if its not a moment object yet
           if ( scope.date && !scope.date._isAMomentObject ) {
             scope.date = moment(scope.date);
           }
+
+          // Bind click events outside directive to close edit popover
+          $document.on('mousedown', function (e) {
+            if ( !!scope.selected && !element[0].contains(e.target) ) {
+              scope.$apply(function () {
+                scope.selected = '';
+                scope.calendar_active = false;
+              });
+            }
+          });
+        }
+      };
+    }
+  };
+}]).
+
+
+/**
+ * Time directive (time input element)
+ */
+directive('timeInput', ['$document', function ($document) {
+  return {
+    restrict: 'E',
+    scope: {
+      time: '=',
+      handler: '&'
+    },
+    replace: true,
+    templateUrl: './time-input.html',
+    compile: function () {
+      return {
+        pre: function preLink() {},
+        post: function postLink(scope, element) {
 
           // Convert time object to moment.js if its not a moment object yet
           if ( scope.time && !scope.time._isAMomentObject ) {
