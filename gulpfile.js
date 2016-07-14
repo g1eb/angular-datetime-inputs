f722dddc28068b3fe026803ac2ed48480495a571
Gleb <git@gleb.nl>
2016-07-14 16:16:16 -0400

    Bundle all uglified js files into a single file in dist folder.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var embedTemplates = require('gulp-angular-embed-templates');
+var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
  
@@ -7,6 +8,7 @@ gulp.task('build', function () {
   gulp.src('src/*.js')
     .pipe(embedTemplates())
     .pipe(uglify())
+    .pipe(concat('datetime-input.js'))
     .pipe(gulp.dest('./dist'));
   gulp.src('src/*.css')
     .pipe(cssnano())
